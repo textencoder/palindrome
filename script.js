c9b7d7c51fc0b8574bf4edf9909af6edcc300f7a
textencoder <160430049+textencoder@users.noreply.github.com>
2024-05-28 21:07:24 -0500

    Version 1

---
+++ script.js
@@ -0,0 +1,24 @@
+const detectiveImage = document.getElementById('detective');
+const trueImage = document.getElementById('true');
+const falseImage = document.getElementById('false');
+const headerText = document.querySelectorAll('h1');
+const inputBox = document.getElementById('text-input');
+const submitBtn = document.getElementById('check-btn');
+
+const isPalindrome = (userInput) => {
+    // filter userInput (regex?)
+    // check if palindrome
+    // return true or false
+}
+
+const displayResult = (isPalindrome) => {
+    //callback: isPalindrome
+    //if true, display green elements
+    //if false, display red elements
+
+    if (isPalindrome) {
+
+    } else {
+
+    }
+}
