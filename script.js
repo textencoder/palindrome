60bceb89432947920a2da73d0152fabcc83aa5df
textencoder <160430049+textencoder@users.noreply.github.com>
2024-05-30 13:28:54 -0500

    Changes made:
    - Added outer glow to input box
    - Main display flex direction changed to column
    - Updated isPalindrome to return true/false dependent on vanilla string input
    - Updated displayResult to manipulate color of DOM elements if true/false
    - Updated displayResult to show/hide DOM elements if true/false
    - isPalindrome & displayResult individually functional in console

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 const detectiveImage = document.getElementById('detective');
 const trueImage = document.getElementById('true');
 const falseImage = document.getElementById('false');
-const headerText = document.querySelectorAll('h1');
+const headerText = document.getElementsByTagName('h1');
 const inputBox = document.getElementById('text-input');
 const submitBtn = document.getElementById('check-btn');
+const resultText = document.getElementById('result');
 
 const isPalindrome = (userInput) => {
     // filter userInput (regex?)
     // check if palindrome
     // return true or false
+
+    let reverseString = userInput.split("").reverse().join("");
+    userInput === reverseString ? true : false;
 }
 
 const displayResult = (isPalindrome) => {
@@ -17,8 +21,29 @@ const displayResult = (isPalindrome) => {
     //if false, display red elements
 
     if (isPalindrome) {
-
+        detectiveImage.style.display = 'none';
+        falseImage.style.display = 'none';
+        trueImage.style.display = 'block';
+        resultText.textContent = "[input] is a palindrome";
+        //headerText.style.color = "#3cb371";
+        //loop through headerText
+        for (i = 0; i < headerText.length; i++) {
+            headerText[i].style.color = '#3cb371';
+          }
+        inputBox.style.borderColor = "#3cb371";
+        inputBox.style.boxShadow = "0 0 10px #3cb371";
+        submitBtn.style.backgroundColor = "#3cb371";
     } else {
-
+        detectiveImage.style.display = 'none';
+        trueImage.style.display = 'none';
+        falseImage.style.display = 'block';
+        resultText.textContent = "[input] is NOT a palindrome";
+        //headerText.style.color = "#f08080";
+        for (i = 0; i < headerText.length; i++) {
+            headerText[i].style.color = '#f08080';
+          }
+        inputBox.style.borderColor = "#f08080";
+        inputBox.style.boxShadow = "0 0 10px #f08080";
+        submitBtn.style.backgroundColor = "#f08080";
     }
 }
