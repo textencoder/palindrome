1d1e2f64de4821a0f81f4215a6c8ced4e0bf66ff
textencoder <160430049+textencoder@users.noreply.github.com>
2024-05-30 19:30:38 -0500

    Version 1.3
    - Added input functionality
    - Refactored HTML/CSS/JS code
    - Introduced event listeners
    - Introduced media queries
    - Increased display font sizes

--- script.js
+++ script.js
@@ -6,44 +6,52 @@ const inputBox = document.getElementById('text-input');
 const submitBtn = document.getElementById('check-btn');
 const resultText = document.getElementById('result');
 
-const isPalindrome = (userInput) => {
-    // filter userInput (regex?)
-    // check if palindrome
-    // return true or false
 
-    let reverseString = userInput.split("").reverse().join("");
-    userInput === reverseString ? true : false;
-}
+submitBtn.addEventListener("click", () => {
+    displayResult(document.getElementById('text-input').value)
+})
 
-const displayResult = (isPalindrome) => {
-    //callback: isPalindrome
-    //if true, display green elements
-    //if false, display red elements
-
-    if (isPalindrome) {
-        detectiveImage.style.display = 'none';
-        falseImage.style.display = 'none';
-        trueImage.style.display = 'block';
-        resultText.textContent = "[input] is a palindrome";
-        //headerText.style.color = "#3cb371";
-        //loop through headerText
-        for (i = 0; i < headerText.length; i++) {
-            headerText[i].style.color = '#3cb371';
-          }
-        inputBox.style.borderColor = "#3cb371";
-        inputBox.style.boxShadow = "0 0 10px #3cb371";
-        submitBtn.style.backgroundColor = "#3cb371";
-    } else {
-        detectiveImage.style.display = 'none';
-        trueImage.style.display = 'none';
-        falseImage.style.display = 'block';
-        resultText.textContent = "[input] is NOT a palindrome";
-        //headerText.style.color = "#f08080";
-        for (i = 0; i < headerText.length; i++) {
-            headerText[i].style.color = '#f08080';
-          }
-        inputBox.style.borderColor = "#f08080";
-        inputBox.style.boxShadow = "0 0 10px #f08080";
-        submitBtn.style.backgroundColor = "#f08080";
+inputBox.addEventListener("keydown", e => {
+    if (e.key === "Enter") {
+        displayResult(document.getElementById('text-input').value);
     }
-}
+})
+
+const displayResult = (userInput) => {
+
+        let inputStr = userInput;
+
+        let reverseString = userInput.split("").reverse().join("");
+    
+        let isPalindrome = (userInput === reverseString);
+
+        if (isPalindrome) {
+            detectiveImage.style.display = 'none';
+            falseImage.style.display = 'none';
+            trueImage.style.display = 'block';
+            resultText.innerHTML = `<span id="inputStr">${inputStr}</span> <br> is a palindrome`;
+            //headerText.style.color = "#3cb371";
+            //loop through headerText
+            for (i = 0; i < headerText.length; i++) {
+                headerText[i].style.color = '#3cb371';
+              }
+            inputBox.style.borderColor = "#3cb371";
+            inputBox.style.boxShadow = "0 0 10px #3cb371";
+            submitBtn.style.backgroundColor = "#3cb371";
+        } else {
+            detectiveImage.style.display = 'none';
+            trueImage.style.display = 'none';
+            falseImage.style.display = 'block';
+            resultText.innerHTML = `<span id="inputStr">${inputStr}</span> <br> is NOT a palindrome`;
+            //headerText.style.color = "#f08080";
+            for (i = 0; i < headerText.length; i++) {
+                headerText[i].style.color = '#f08080';
+              }
+            inputBox.style.borderColor = "#f08080";
+            inputBox.style.boxShadow = "0 0 10px #f08080";
+            submitBtn.style.backgroundColor = "#f08080";
+        }
+    }
+
+
+
