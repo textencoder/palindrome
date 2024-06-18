625050ceae963a985e1b68c9b6957e64edf56009
textencoder <160430049+textencoder@users.noreply.github.com>
2024-06-18 13:31:09 -0500

    Version 1.4
    - Consolidated & centered header text
    - Adjusted font sizes
    - Removed unnecessary CSS code
    - Removed for loop through header text
    - Removed unnecessary JS code

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const detectiveImage = document.getElementById('detective');
 const trueImage = document.getElementById('true');
 const falseImage = document.getElementById('false');
-const headerText = document.getElementsByTagName('h1');
+const headerText = document.getElementById('header');
 const inputBox = document.getElementById('text-input');
 const submitBtn = document.getElementById('check-btn');
 const resultText = document.getElementById('result');
@@ -25,16 +25,17 @@ const displayResult = (userInput) => {
     
         let isPalindrome = (inputStr === reverseString);
 
+        // Do not accept invalid input
+        // if (userInput === "") {
+        //    resultText.innerText = "Please enter a valid input";
+        // }
+
         if (isPalindrome) {
             detectiveImage.style.display = 'none';
             falseImage.style.display = 'none';
             trueImage.style.display = 'block';
             resultText.innerHTML = `<span id="inputStr">${inputStr}</span> <br> is a palindrome`;
-            //headerText.style.color = "#3cb371";
-            //loop through headerText
-            for (i = 0; i < headerText.length; i++) {
-                headerText[i].style.color = '#3cb371';
-              }
+            headerText.style.color = '#3cb371'
             inputBox.style.borderColor = "#3cb371";
             inputBox.style.boxShadow = "0 0 10px #3cb371";
             submitBtn.style.backgroundColor = "#3cb371";
@@ -43,10 +44,7 @@ const displayResult = (userInput) => {
             trueImage.style.display = 'none';
             falseImage.style.display = 'block';
             resultText.innerHTML = `<span id="inputStr">${inputStr}</span> <br> is NOT a palindrome`;
-            //headerText.style.color = "#f08080";
-            for (i = 0; i < headerText.length; i++) {
-                headerText[i].style.color = '#f08080';
-              }
+            headerText.style.color = '#f08080'
             inputBox.style.borderColor = "#f08080";
             inputBox.style.boxShadow = "0 0 10px #f08080";
             submitBtn.style.backgroundColor = "#f08080";
